85466eb35884cc871f15a760d70bcc4aebe14b5d
Khushi Mehta <khushimehta8282@gmail.com>
2023-03-04 12:57:05 +0530

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,58 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import SimpleLayout from './layouts/simple';
+//
+import BlogPage from './pages/BlogPage';
+import UserPage from './pages/UserPage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
+import Page404 from './pages/Page404';
+import ProductsPage from './pages/ProductsPage';
+import DashboardAppPage from './pages/DashboardAppPage';
+import BarcodePage from './pages/BarcodePage';
+import CSVUploadPage from './pages/CSVUploadPage';
+import Kanban from "./pages/Kanban"
+import LabTabs from './sections/@dashboard/tabs/Tabs';
+
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: 'app', element: <DashboardAppPage /> },
+        { path: 'user', element: <UserPage /> },
+        { path: 'product', element: <ProductsPage /> },
+        { path: 'blog', element: <BlogPage /> },
+        { path: 'scanbarcode', element: <LabTabs /> },
+        { path: 'schedule', element: <Kanban /> },
+      ],
+    },
+    {
+      path: 'login',
+      element: <LoginPage />,
+    },
+    {
+      path: 'signup',
+      element: <SignUpPage />,
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: '404', element: <Page404 /> },
+        { path: '*', element: <Navigate to="/404" /> },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+  ]);
+
+  return routes;
+}
